9c1216c3d1418db18f58fa466f2397489abacfb7
Elforax <justingort2@gmail.com>
2022-01-31 13:33:48 +0100

    Added Logging support with custom format

--- upnpmanager.py
+++ upnpmanager.py
@@ -3,5 +3,8 @@
 
 import logging
 
+# global logger settings
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-
+log = logging.getLogger("Brickkeeper")
+log.setLevel(logging.DEBUG)
